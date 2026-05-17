bd57663f02e0db78110642309c2ca98a8fbe3c26
neervasa00000000 <156447490+neervasa00000000@users.noreply.github.com>
2026-05-17 21:33:36 +1000

    Remove crypto checkout system and blockchain folder.
    Reverts Pay with Crypto integration, Netlify crypto functions,
    crypto-checkout static pages, and all blockchain payment code.

--- blockchain/src/shipping.js
+++
@@ -1,3 +0,0 @@
-const path = require('path');
-
-module.exports = require(path.join(__dirname, '../../scripts/global/shipping-rates.js'));
--- netlify/functions/crypto-api.js
+++
@@ -1,6 +0,0 @@
-process.env.NETLIFY = 'true';
-
-const serverless = require('serverless-http');
-const app = require('../../blockchain/src/app');
-
-module.exports.handler = serverless(app);
